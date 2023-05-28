9e0199e04fd42f2dbdf9730c0ac92902e38ed913
kpavkin <kpavkin@web3tech.ru>
2023-05-28 15:19:03 +0300

    placements tree

--- src/apply.rs
+++ src/apply.rs
@@ -2,10 +2,7 @@ pub trait Apply<D> {
     fn apply(&mut self, diff: D);
 }
 
-impl<T> Apply<T> for T
-where
-    T: Clone,
-{
+impl<T> Apply<T> for T {
     fn apply(&mut self, diff: T) {
         *self = diff;
     }
